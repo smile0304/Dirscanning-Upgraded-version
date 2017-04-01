1d585be057e828eabbd42ea628814eca014a599e
smile0304 <smile@smilehacker.net>
2017-04-01 15:46:04 +0800

    修正一些东西

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Dirscanning-Upgraded-version" 
--- lib/config/config.py
+++ lib/config/config.py
@@ -2,4 +2,4 @@ top = 50
 skip = 0
 
 # Modify the key to your primary account
-accountKey = "uSimFljEMwAXA1uvatEiYJsb+PLnwJxwJ56iBq1wiyI"
+accountKey = ""
